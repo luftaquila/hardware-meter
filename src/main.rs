d3d3a3ca97b4b1792508d45c86442c19b480b321
오병준 <mail@luftaquila.io>
2024-05-08 00:28:43 +0900

    add: write to serial port

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
-use std::{thread, time};
+use std::{env, thread, time};
 use sysinfo::System;
 
 fn main() {
+    let ports = serialport::available_ports().expect("No ports found!");
+    for p in ports {
+        println!("{:?}", p);
+    }
     let mut sys = System::new();
+
+    /* parse port from command line arguments */
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("[ERR] no port specified");
+        return;
+    }
+
+    let port = &args[1];
+    let mut serial = serialport::new(port, 115200)
+        .timeout(time::Duration::from_millis(10))
+        .open()
+        .expect("[ERR] cannot open port");
+
     sys.refresh_cpu_usage();
 
     loop {
-        thread::sleep(time::Duration::from_secs(1));
+        thread::sleep(time::Duration::from_millis(100));
         sys.refresh_cpu_usage();
-        println!("{:?}", sys.global_cpu_info().cpu_usage());
+
+        let usage = sys.global_cpu_info().cpu_usage();
+        serial
+            .write(&usage.to_be_bytes())
+            .expect("[ERR] write failed");
     }
 }
