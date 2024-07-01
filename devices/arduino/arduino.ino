6252b21ababeaed4092a5916bcb3b06f3a3a53e5
오병준 <mail@luftaquila.io>
2024-07-01 19:58:13 +0900

    update: arduino implementation

--- devices/arduino/arduino.ino
+++ devices/arduino/arduino.ino
@@ -1,4 +1,6 @@
-#define PWM 3
+// common PWM pins shared by various arduino series
+const int pwm[] = { 3, 5, 6, 9, 10, 11 };
+const int pwm_cnt = (sizeof(pwm) / sizeof(const int));
 
 union f32 {
   byte bytes[4];
@@ -17,8 +19,22 @@ void loop() {
       f.bytes[i] = Serial.read();
     }
 
-    if (f.bytes[0] == 100) {
-      analogWrite(PWM, int(2.55 * f.value));
+    /*
+     * packet value is a 4 byte little endian float percentage
+     * first byte in the packet designates the pwm output port
+     *
+     * |       < PACKET STRUCTURE >       |
+     * |  byte 0  byte 1  byte 2  byte 3  |
+     * | | port |                         |
+     * | |   value (float percentage)   | |
+     *
+     * port uses the part of the fraction bits of the IEEE 754 float structure
+     * this sacrifies the precision of the value, but it won't visible in the meter
+     */
+
+    if (f.bytes[0] < pwm_cnt) {
+      // analogWrite max 255 when f.value is 100.0 %
+      analogWrite(pwm[f.bytes[0]], int(2.55 * f.value));
     }
   }
 }
